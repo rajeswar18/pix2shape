e7af5a7faad7dd7021da8d971a01c6f2b59c19ce
Vikram Voleti <vikky2904@gmail.com>
2018-11-20 17:06:39 -0500

    Fixed iterator for reading images, cam_pos, light_pos from dir

--- diffrend/torch/GAN/iterator.py
+++ diffrend/torch/GAN/iterator.py
@@ -1,79 +1,60 @@
-import os
-import numpy as np
 import glob
-import pickle as pkl
+import numpy as np
+import os
 
 from scipy.misc import imread
 
 
 class Iterator(object):
 
-    # def __init__(self, root_dir="/home/sai", img_path = 'bamboohouse_final2',
-    #              cam_path='cam',light_path='light',
-    #              batch_size=6, nb_sub=None):
-
-        # self.root_dir = root_dir
-        # self.img_path = os.path.join(root_dir, img_path)
-        # self.campos_path = os.path.join(root_dir, cam_path)
-        # self.lightpos_path = os.path.join(root_dir, light_path)
-        # self.batch_size = batch_size
-        # self.batch_idx = 0
-        # self.imgs = glob.glob(self.img_path + "/*light*")
-
-        # if nb_sub is not None:
-        #     self.imgs = self.imgs[:nb_sub]
-
-    def __init__(self, root_dir, batch_size=6, nb_sub=None):
+    def __init__(self, root_dir, batch_size=6, shuffle=False, nb_sub=None):
 
         self.root_dir = root_dir
         self.batch_size = batch_size
+        self.shuffle = shuffle
         self.batch_idx = 0
-        self.image_paths = glob.glob(os.path.join(self.root_dir, '*.png'))
+
+        self.image_paths = sorted(glob.glob(os.path.join(self.root_dir, 'img',  '*.png')))
         self.cam_pos = np.loadtxt(os.path.join(self.root_dir, 'cam_pos.csv'), delimiter=',')
         self.light_pos = np.loadtxt(os.path.join(self.root_dir, 'light_00_pos.csv'), delimiter=',')
 
         if nb_sub is not None:
             self.image_paths = self.image_paths[:nb_sub]
+            self.cam_pos = self.cam_pos[:nb_sub]
+            self.light_pos = self.light_pos[:nb_sub]
 
-    # def _get_img(self, i):
+        # Indices for returning batches, possibly shuffled order of images
+        self.idx = np.arange(len(self))
 
-    #     light_path = self.imgs[i]
-    #     # img = Image.open(img_path)
-    #     # img_array = np.array(img)
+        if self.shuffle:
+            np.random.shuffle(self.idx)
 
-    #     cam_path = light_path[:-10]+"_cam.npy"
-    #     im_path = light_path[:-10]+".npy"
-
-    #     cam= np.load(cam_path)
-    #     light=np.load(light_path)
-    #     img_array=np.load(im_path)
-
-    #     return img_array, cam, light
-
-    def _get_img(self, i):
-        return imread(self.image_paths[i % len(self.image_paths)]), self.cam_pos[i % len(self.cam_pos)], self.light_pos[i % len(self.light_pos)]
+        # Current index for batches
+        self.batch_idx = 0
 
     def __len__(self):
-        return len(self.imgs)
+        return len(self.image_paths)
 
-    def __getitem__(self, key):
-        if isinstance(key, slice):
-            # Get the start, stop, and step from the slice
-            res = [self[ii] for ii in range(*key.indices(len(self)))]
-            xs, ys, caps = zip(*[x for x in res if x is not None])
-            return np.array(xs), np.array(ys), np.array(caps)
-        elif isinstance(key, int):
-            if key < 0:  # Handle negative indices
-                key += len(self)
-            if key < 0 or key >= len(self):
-                print("The index (%d) is out of range.")
-            return self._get_img(key)  # Get the data from elsewhere
-        else:
-            print("Invalid argument type.")
+    def read_image(self, image_path):
+        return imread(image_path)
 
     def __iter__(self):
-        for batch_idx in range(int(len(self)/self.batch_size)):
-            if (batch_idx+1)*self.batch_size < len(self):
-                yield self[batch_idx*self.batch_size: (batch_idx+1)*self.batch_size]
-            else:
-                yield self[batch_idx * self.batch_size:]
+        return self
+
+    def __next__(self):
+
+        if self.batch_idx + self.batch_size - 1 > len(self):
+            # Reset
+            self.batch_idx = 0
+            # Shuffle indices again
+            if self.shuffle:
+                np.random.shuffle(self.idx)
+
+        # Return batch of images, cam_pos, light_pos
+        batch_images = [imread(self.image_paths[idx])[:, :, :3] for idx in self.idx[self.batch_idx:self.batch_idx+self.batch_size]]
+        batch_cam_pos = [self.cam_pos[idx] for idx in self.idx[self.batch_idx:self.batch_idx+self.batch_size]]
+        batch_light_pos = [self.light_pos[idx] for idx in self.idx[self.batch_idx:self.batch_idx+self.batch_size]]
+
+        self.batch_idx += self.batch_size
+
+        return batch_images, batch_cam_pos, batch_light_pos
