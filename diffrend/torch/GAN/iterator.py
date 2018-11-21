15ee9a9c4a183208be68f44693eb53fa4fc5aaf4
Vikram Voleti <vikky2904@gmail.com>
2018-11-20 21:43:55 -0500

    Return img, depth_img, cam_pos (instead of img, cam_pos, light_pos)

--- diffrend/torch/GAN/iterator.py
+++ diffrend/torch/GAN/iterator.py
@@ -15,11 +15,13 @@ class Iterator(object):
         self.batch_idx = 0
 
         self.image_paths = sorted(glob.glob(os.path.join(self.root_dir, 'img',  '*.png')))
+        self.depth_image_paths = sorted(glob.glob(os.path.join(self.root_dir, 'depth',  '*.png')))
         self.cam_pos = np.loadtxt(os.path.join(self.root_dir, 'cam_pos.csv'), delimiter=',')
         self.light_pos = np.loadtxt(os.path.join(self.root_dir, 'light_00_pos.csv'), delimiter=',')
 
         if nb_sub is not None:
             self.image_paths = self.image_paths[:nb_sub]
+            self.depth_image_paths = self.depth_image_paths[:nb_sub]
             self.cam_pos = self.cam_pos[:nb_sub]
             self.light_pos = self.light_pos[:nb_sub]
 
@@ -52,9 +54,10 @@ class Iterator(object):
 
         # Return batch of images, cam_pos, light_pos
         batch_images = [imread(self.image_paths[idx])[:, :, :3] for idx in self.idx[self.batch_idx:self.batch_idx+self.batch_size]]
+        batch_depth_images = [imread(self.depth_image_paths[idx])[:, :, :3] for idx in self.idx[self.batch_idx:self.batch_idx+self.batch_size]]
         batch_cam_pos = [self.cam_pos[idx] for idx in self.idx[self.batch_idx:self.batch_idx+self.batch_size]]
-        batch_light_pos = [self.light_pos[idx] for idx in self.idx[self.batch_idx:self.batch_idx+self.batch_size]]
+        # batch_light_pos = [self.light_pos[idx] for idx in self.idx[self.batch_idx:self.batch_idx+self.batch_size]]
 
         self.batch_idx += self.batch_size
 
-        return batch_images, batch_cam_pos, batch_light_pos
+        return batch_images, batch_depth_images, batch_cam_pos
